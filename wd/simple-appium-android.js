fdbb8a2e7610fe24a44acccc57248a629980a32c
ndmanvar <ndmanvar@yahoo.com>
2016-09-28 09:32:21 -0700

    using https

--- wd/simple-appium-android.js
+++ wd/simple-appium-android.js
@@ -7,9 +7,8 @@ var wd = require('wd'),
         platformName: 'Android',
         name: 'Sample Test',
         app: 'http://appium.s3.amazonaws.com/ContactManager.apk'
-    };
-
-browser = wd.remote("ondemand.saucelabs.com", 80, "YOUR_SAUCE_USERNAME", "YOUR_SAUCE_ACCESS_KEY");
+    },
+    browser = wd.remote("https://" + "YOUR_SAUCE_USERNAME" + ":" + "YOUR_SAUCE_ACCESS_KEY" + "@" + "ondemand.saucelabs.com:443/wd/hub");
 
 browser.init(desiredCaps, function() {
     browser.quit();
